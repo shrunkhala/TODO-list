cb8b5050948694237d42372af6244546a2c969a7
Shrunkhala <shrunkhalajawade@gmail.com>
2021-08-19 12:42:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { useState } from 'react';
 
 function App() {
+
+  const[newtask , setnewtask] = useState('')
+  const[tasks , settask] = useState([])
+
+
+  function addtask(){
+
+    settask([...tasks , newtask])
+
+  }
+  function deletetask(index){
+
+    var duplicatearray = [...tasks]
+    duplicatearray.splice(index , 1)
+    settask(duplicatearray)
+
+  }
+
+  const taskslist = tasks.map((object, index)=>{
+
+    return <div className='row justify-content-center'>
+
+      <h3 className='col-md-6 text-start'>{index+1}. {object}</h3>
+      <button onClick={()=>{deletetask(index)}} className='col-md-1 btn btn-danger m-1'>DELETE</button>
+
+    </div>
+
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+      <br />
+      <h1>Todo List</h1>
+      
+      
+      <div className='row justify-content-center'>
+
+        <input type="text" placeholder='Taskname' className='form-content col-md-6 m-1'
+        value={newtask} onChange={(e)=>{setnewtask(e.target.value)}}/>
+        <button onClick={addtask} className='btn btn-primary col-md-1 m-1'>ADD TASK</button>
+
+      </div>
+
+      <br />
+      
+      {taskslist}
+     
+      
+      
     </div>
+    
   );
 }
 
